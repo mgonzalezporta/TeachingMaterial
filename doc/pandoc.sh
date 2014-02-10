b44dd5d8d120abb3806b7fbc2c30fa9e0a6cd5eb
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2014-02-10 21:11:50 +0000

    pdf updated

--- doc/pandoc.sh
+++ doc/pandoc.sh
@@ -15,4 +15,4 @@
 #     --variable urlcolor:black 
 #     --variable geometry:margin=3cm
 
-pandoc *.md -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical - San Michele all'Adige (Trento), Italy" --variable date:"2013/07/02" --variable author:"Mar Gonzàlez-Porta" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
+pandoc *.md -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical - Agricultural Omics, EBI" --variable date:"2014/02/19" --variable author:"Mar Gonzàlez-Porta" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
