54eb0225e057a4e610bca8491bc84a20c31590b3
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2014-08-27 11:40:31 +0100

    generated practical pdf

--- doc/pandoc.sh
+++ doc/pandoc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# IMPORTANT: always leave two empty spaces at the end of each md file,
+# IMPORTANT: always leave two empty lines at the end of each md file,
 #			 otherwise pandonc doesn't distinguish the sections properly
 #
 # human readable command:
 # pandoc *.md 
 #     -o ../pdf/practical.pdf 
 #     --toc 
-#     --variable title:"RNA-seq data analysis practical - San Michele all'Adige (Trento), Italy"
+#     --variable title:"RNA-seq data analysis practical"
 #     --variable date:"2013/07/02"
 #     --variable author:"Mar Gonzàlez-Porta"
 #     --variable links-as-notes 
@@ -15,4 +15,4 @@
 #     --variable urlcolor:black 
 #     --variable geometry:margin=3cm
 
-pandoc *.md -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical - San Michele all'Adige (Trento), Italy" --variable date:"2013/07/02" --variable author:"Mar Gonzàlez-Porta" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
+pandoc *.md -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical" --variable date:"2014/08/27" --variable author:"Mar Gonzàlez-Porta" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
